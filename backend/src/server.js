4d2fdadd57eb5604555034213f5d7c10426deea5
ashif-costa0770 <ashifcostatechnolab@gmail.com>
2026-02-17 16:42:43 +0530

    Frontend url updated in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    origin: process.env.FRONTEND_URL || "http://werentify.duckdns.org/",
+    origin: process.env.FRONTEND_URL || "http://werentify-newfrontend-sb3idl-57c17f-40-160-2-18.traefik.me",
     credentials: true,
   }),
 );
