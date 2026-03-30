0d353a2f0951d55f7741978c773c044d112f672f
ashif-costa0770 <ashifcostatechnolab@gmail.com>
2026-03-30 10:53:24 +0530

    google login error fixing

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,6 +41,7 @@ import FaqItem from "./models/help/faqItem.model.js";
 import contactRoutes from "./routes/contact.route.js";
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 const configuredOrigins = [
@@ -55,7 +56,7 @@ const configuredOrigins = [
 
 /* MIDDLEWARES */
 /* -------------------------------------------------- */
-
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(compression());
 app.use(cookieParser());
@@ -74,6 +75,7 @@ app.use(
 );
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 /* Passport Session (Google Auth) */
